5f275750f568ce98fb62f2d07bf869ed9e7a6fb1
mayjoonll <mayjoonll@gmail.com>
2025-09-10 13:25:33 +0900

    entity변경

--- src/main/java/com/my/touristAttraction/controller/TouristSpotController.java
+++ src/main/java/com/my/touristAttraction/controller/TouristSpotController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tourist-spots")
+@RequestMapping("/touristSpots")
 @RequiredArgsConstructor
 public class TouristSpotController {
 
